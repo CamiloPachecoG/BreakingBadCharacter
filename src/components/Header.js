1530141c36f3beed5d4a774fd837fc6ea59bf40e
CamiloPachecoG <58574070+CamiloPachecoG@users.noreply.github.com>
2021-02-03 21:10:11 -0300

    Fix urls

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../img/logo.png';
+//import logo from '../img/logo.png';
 
 const Header = () => {
     return (
         <>
             {/* Header */}
             <header className='center animate__animated animate__jackInTheBox'>
-                <img src={ logo } alt='' />
+                <img src='https://res.cloudinary.com/dk3yfcnpo/image/upload/v1612394548/gdiad9armt4utzvls0qc.png' alt='breaking-bad' />
             </header>
         </>
     )
